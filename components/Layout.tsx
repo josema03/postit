e696669833a3971b1727f9101af80f2c81568844
josema03 <jose.marin1997@gmail.com>
2021-04-07 19:27:54 -0400

    style: change style of content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,24 +2,43 @@
 import React from "react";
 import NavBar from "./NavBar";
 import styled from "styled-components";
-import { Container } from "@material-ui/core";
 
-const StyledContainer = styled(Container).attrs(() => ({
-  maxWidth: "sm",
-}))`
+const StyledBody = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  min-height: 100vh;
+  min-width: 100%;
+  max-width: 100%;
+  padding: 0px;
+  margin: 0px;
+  overflow: auto;
+`;
+
+const StyledContent = styled.div`
+  display: flex;
+  flex-grow: 1;
+  max-width: 100%;
   align-items: center;
-  padding: 12px;
+  justify-content: center;
+`;
+
+const StyledMain = styled.main`
+  display: flex;
+  margin: 10px 0px;
+  min-width: 75%;
+  max-width: 75%;
+  flex-grow: 1;
+  flex-direction: column;
 `;
 
 const Layout: React.FC<unknown> = ({ children }) => {
   return (
-    <>
+    <StyledBody>
       <NavBar />
-      <StyledContainer>{children}</StyledContainer>
-    </>
+      <StyledContent>
+        <StyledMain>{children}</StyledMain>
+      </StyledContent>
+    </StyledBody>
   );
 };
 
