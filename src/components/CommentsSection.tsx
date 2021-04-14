a7ce60fa013894986c272f5d4d7ac41db1bd79af
josema03 <jose.marin1997@gmail.com>
2021-04-14 17:47:47 -0400

    feat: show comments on UI

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,5 +1,8 @@
+import { Box, Typography } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
+import { useCommentsQuery } from "../graphql/generated/graphql";
+import useGetPostFromRoute from "../utils/useGetPostFromRoute";
 import PostComment from "./PostComments";
 
 const StyledCardBody = styled.div`
@@ -7,10 +10,50 @@ const StyledCardBody = styled.div`
   padding: 10px;
 `;
 
+const StyledComment = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 100%;
+  margin: 5px;
+`;
+
 const CommentsSection: React.FC = () => {
+  const { data: postData } = useGetPostFromRoute();
+  const { data, loading } = useCommentsQuery({
+    variables: {
+      postId: postData.post.id,
+      limit: 10,
+      parentPath: "/",
+    },
+  });
+
+  console.log(data, loading);
+
+  const comments = data?.comments?.map((comment) => {
+    return (
+      <StyledComment key={comment.id}>
+        <Box display="flex" alignItems="flex-end">
+          <Typography variant="subtitle2">{comment.user.username}</Typography>
+          <Box marginLeft="5px">
+            <Typography variant="caption" color="textSecondary">
+              At {comment.createdAt}
+            </Typography>
+          </Box>
+        </Box>
+        <Box>
+          <Typography variant="body2">{comment.comment}</Typography>
+        </Box>
+      </StyledComment>
+    );
+  });
+
   return (
     <StyledCardBody>
       <PostComment />
+      <Box display="flex" flexDirection="column">
+        {comments}
+      </Box>
     </StyledCardBody>
   );
 };
